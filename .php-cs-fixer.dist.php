2c2a9fb0614ceaf646304ebe0331c0a0804b7f6f
Alexis Wurth <alexis.wurth@sensiolabs.com>
2025-07-01 18:06:20 +0200

    php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,52 @@
+<?php
+
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+;
+
+return (new PhpCsFixer\Config())
+    ->setUnsupportedPhpVersionAllowed(true)
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR1' => true,
+        '@PSR2' => true,
+        '@PSR12' => true,
+        '@PSR12:risky' => true,
+        '@PhpCsFixer' => true,
+        // '@PhpCsFixer:risky' => true,
+        // '@Symfony' => true,
+        // '@Symfony:risky' => true,
+        '@PHP84Migration' => true,
+        'array_indentation' => true,
+        'compact_nullable_type_declaration' => true,
+        // 'declare_strict_types' => true,
+        // 'fully_qualified_strict_types' => [
+        //     'import_symbols' => true,
+        // ],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'native_function_invocation' => [
+            'include' => ['@internal'],
+        ],
+        'no_superfluous_elseif' => true,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'nullable_type_declaration_for_default_null_value' => false,
+        'return_assignment' => true,
+        'strict_param' => true,
+        'trailing_comma_in_multiline' => [
+            'elements' => ['arguments', 'arrays', 'match', 'parameters'],
+        ],
+        'void_return' => true,
+        'yoda_style' => [
+            'always_move_variable' => true,
+        ],
+    ])
+    ->setFinder($finder)
+;
