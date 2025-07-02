409b451e96748a1bc1746e93be15d02d31202d1a
Alexis Wurth <alexis.wurth@sensiolabs.com>
2025-07-02 21:40:39 +0200

    rector

---
+++ rector.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+
+return RectorConfig::configure()
+    ->withParallel()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withImportNames()
+    ->withAttributesSets()
+    ->withPreparedSets(
+        // deadCode: true,
+        // codeQuality: true,
+        // typeDeclarations: true,
+        privatization: true,
+        instanceOf: true,
+        earlyReturn: true,
+        // strictBooleans: true,
+        // rectorPreset: true,
+    )
+    // ->withComposerBased(phpunit: true)
+    // ->withPhpSets()
+    // ->withSets([
+    //     PHPUnitSetList::PHPUNIT_110,
+    // ])
+;
