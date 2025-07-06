45437b17445cc41d5cfb8e641185de0e4453fbfd
Alexis Wurth <alexis.wurth@sensiolabs.com>
2025-07-06 11:44:53 +0200

    tail

--- src/Functional/Tail.php
+++ src/Functional/Tail.php
@@ -14,7 +14,12 @@ namespace Functional;
  * Returns all items from $collection except first element (head). Preserves $collection keys.
  * Takes an optional callback for filtering the collection.
  *
- * @param iterable<mixed, mixed> $collection
+ * @template TKey
+ * @template TValue
+ *
+ * @param iterable<TKey, TValue> $collection
+ *
+ * @return array<TKey, TValue>
  *
  * @no-named-arguments
  */
@@ -30,7 +35,7 @@ function tail(iterable $collection, ?callable $callback = null): array
             continue;
         }
 
-        if (!$callback || $callback($element, $index, $collection)) {
+        if (null === $callback || $callback($element, $index, $collection)) {
             $tail[$index] = $element;
         }
     }
